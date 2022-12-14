03fb851ef6b98a4edd6c366f2d1919620e23c1e0
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-14 17:18:59 +0100

    Fix prettier postinstall

--- packages/prettier/postinstall.sh
+++ packages/prettier/postinstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-lib=@r1oga/config-prettier
-
-echo "\"$lib\"" > "$INIT_CWD/.prettierrc.yaml"
+echo "\"@r1oga/prettier-config\"" > "$INIT_CWD/.prettierrc.yaml"
 cp .prettierignore "$INIT_CWD/.prettierignore"
