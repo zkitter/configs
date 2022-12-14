5fdd54319151dc25d093090edbb8e23192a6becc
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-14 17:09:13 +0100

    fix eslint package name
    Required to be named `../eslint-config`
    
    https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules

--- packages/eslint/postinstall.sh
+++ packages/eslint/postinstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-lib=@r1oga/config-eslint
-
-echo "extends: $lib" > "$INIT_CWD/.eslintrc.yaml"
+# see sharing eslint config & npm scoped modules https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules
+echo "extends: ['@r1oga']" > "$INIT_CWD/.eslintrc.yaml"
 cp .eslintignore "$INIT_CWD/.eslintignore"
