a516a3e290e08cf1c9e3b8d6ddb95f65fa4f4574
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-20 11:00:10 +0100

    Fix postinstall scripts

--- packages/eslint/postinstall.sh
+++ packages/eslint/postinstall.sh
@@ -3,11 +3,6 @@
 # see sharing eslint config & npm scoped modules https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules
 if [[ ! -f  "$INIT_CWD/.eslintrc.yaml" ]];then
   echo "extends: ['@r1oga']" > "$INIT_CWD/.eslintrc.yaml"
-else
-    read -p "overwrite .eslintrc.yaml?(Yn) " overwrite
-    if [[  -z "$overwrite" || "$overwrite" -eq "Y" || "$overwrite" -eq "y" ]];then
-      echo "extends: ['@r1oga']" > "$INIT_CWD/.eslintrc.yaml"
-    fi
 fi
 
-cp -i .eslintignore "$INIT_CWD/.eslintignore"
+cp -n .eslintignore "$INIT_CWD/.eslintignore"
--- packages/prettier/postinstall.sh
+++ packages/prettier/postinstall.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
+#echo "HERE"
 if [[ ! -f "$INIT_CWD/.prettierrc.yaml" ]];then
   echo "'@r1oga/prettier-config'" > "$INIT_CWD/.prettierrc.yaml"
-else
-  # shellcheck disable=SC2162
-  read -p "overwrite .prettierrc.yaml?(Yn) " overwrite
-  if [[  -z "$overwrite" || "$overwrite" -eq "Y" || "$overwrite" -eq "y" ]];then
-      echo "'@r1oga/prettier-config'" > "$INIT_CWD/.prettierrc.yaml"
-  fi
 fi
 
-cp -i .prettierignore "$INIT_CWD/.prettierignore"
+cp -n .prettierignore "$INIT_CWD/.prettierignore"
