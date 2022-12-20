507e65c44a80e05a0d79ded693c6932c6c8e9403
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-20 13:18:05 +0100

    chore: Update to zkitter style (#1)
    * chore: Update to zkitter style
    
    * Update postinstall scripts
    
    * Update package.json
    
    * Update package.json

--- packages/prettier/postinstall.sh
+++ packages/prettier/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "HERE"
 if [[ ! -f "$INIT_CWD/.prettierrc.yaml" ]];then
-  echo "'@r1oga/prettier-config'" > "$INIT_CWD/.prettierrc.yaml"
+  echo "'@zkitter/prettier-config'" > "$INIT_CWD/.prettierrc.yaml"
 fi
 
 cp -n .prettierignore "$INIT_CWD/.prettierignore"
