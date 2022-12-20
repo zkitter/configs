ddb634abc8eaa818383884fafd785bba1fa04476
r1oga <38692952+r1oga@users.noreply.github.com>
2022-12-20 11:23:10 +0100

    Update postinstall scripts

--- packages/eslint/postinstall.sh
+++ packages/eslint/postinstall.sh
@@ -2,7 +2,7 @@
 
 # see sharing eslint config & npm scoped modules https://eslint.org/docs/latest/developer-guide/shareable-configs#npm-scoped-modules
 if [[ ! -f  "$INIT_CWD/.eslintrc.yaml" ]];then
-  echo "extends: ['@r1oga']" > "$INIT_CWD/.eslintrc.yaml"
+  echo "extends: ['@zkitter']" > "$INIT_CWD/.eslintrc.yaml"
 fi
 
 cp -n .eslintignore "$INIT_CWD/.eslintignore"
